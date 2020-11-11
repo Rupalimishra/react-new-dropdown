4384bcd3ab1d43dc5a0294d680f14ab60e8d4e15
Rupalimishra <rupalimishra389@gmail.com>
2020-11-11 21:49:20 +0530

    lastcommit

--- src/index.js
+++ src/index.js
@@ -4,114 +4,121 @@ import ReactDOM from 'react-dom';
 
 import './style.css';
 class App extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.handleKeyDown = this.handleKeyDown.bind(this)
-      this.handleClick = this.handleClick.bind(this);
-      this.handleOutsideClick = this.handleOutsideClick.bind(this);
-  
-      this.state = {
-        popupVisible: false,
-        selectedvalue: "dropdown",
-        cursor: 0,
-        result: [
-        "Item1", "Item2", "Item3", "Item4"
-        ]
 
-      }
+    // this.textInput = React.createRef();
+    // this.focusTextInput = this.focusTextInput.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleOutsideClick = this.handleOutsideClick.bind(this);
+
+    this.state = {
+      popupVisible: false,
+      selectedvalue: "dropdown",
+      cursor: 0,
+      result: [
+        "Neptunium", "Plutonium", "Americium", "Berkelium"
+      ]
+
     }
-  
-    handleClick() {
-      
-      if (!this.state.popupVisible) {
-       
-        document.addEventListener('click', this.handleOutsideClick, false);
-      } else {
-        document.removeEventListener('click', this.handleOutsideClick, false);
-      }
-  
-      this.setState(prevState => ({
-         popupVisible: !prevState.popupVisible,
-      }));
+   
+  }
+
+  handleClick() {
+
+    if (!this.state.popupVisible) {
+
+      document.addEventListener('click', this.handleOutsideClick, false);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick, false);
     }
-    
-    handleOutsideClick(e) {
+
+    this.setState(prevState => ({
+      popupVisible: !prevState.popupVisible,
+    }));
+  }
+
+  handleOutsideClick(e) {
+    if (e.target.localName == "button") {
       this.setState({
         selectedvalue: 'dropdown'
-       })
-      console.log(e.target.innerText )
-
-      this.setState({
-       selectedvalue: e.target.innerText
       })
-    
-      if (this.node.contains(e.target)) {
-        return;
-      }
+    }
+    console.log("from outside", e.target.innerText)
+    if (e.target.localName == "button") {
+      this.setState({
+        selectedvalue: e.target.innerText
+      })
+    }
+   
+       this.setState(prevState => ({
+      popupVisible: false,
+
+    }));
+   
+   
+    this.handleClick();
+  }
+
+  handleKeyDown(e) {
+    const { cursor, result } = this.state
+    console.log("hii" + this.state.cursor)
+    if (e.keyCode == 38 && cursor >= 0 ) {
       this.setState(prevState => ({
-        popupVisible: !prevState.popupVisible,
-     }));
-      this.handleClick();
+        cursor: this.state.cursor - 1,
+        selectedvalue: result[cursor]
+      }))
+      if (cursor == result.length) {
+        this.setState({ selectedvalue: "dropdown" })
+      }
+      console.log("if" + this.state.cursor)
+    } else if (e.keyCode == 40 && cursor < result.length) {
+      this.setState(prevState => ({
+        cursor: this.state.cursor + 1,
+        selectedvalue: result[cursor]
+      }))
+      console.log("elseif" + this.state.cursor)
     }
 
-    handleKeyDown(e) {
-      const { cursor, result } = this.state
-     console.log("hii" + this.state.cursor)
-      if (e.keyCode == 38 && cursor >= 0) {
-        this.setState( prevState => ({
-          cursor: this.state.cursor - 1,
-          selectedvalue: result[cursor]
-        }))
-       if (cursor == result.length){
-        this.setState({selectedvalue:"dropdown"})
-       }
-      console.log("if" +this.state.cursor)
-      }  else if (e.keyCode == 40 && cursor < result.length) {
-        this.setState( prevState => ({
-          cursor: this.state.cursor + 1,
-          selectedvalue: result[cursor]
-        }))
-      console.log("elseif" +this.state.cursor)
-      }
-    
   }
-  
-  
-    
-
+  // focusTextInput() {
    
-  
-    render() {
-     
-      return (
-        <div className="popover-container" ref={node => { this.node = node; } } onKeyDown={this.handleKeyDown }>
-          <button
-            onClick={this.handleClick  }
-          >
-           {this.state.selectedvalue}
+  //   this.textInput.current.focus();
+  // }
+ 
+
+
+
+
+
+  render() {
+    console.log(this.state.selectedvalue);
+    return (
+      <div className="popover-container" ref={node => { this.node = node; }} onKeyDown={this.handleKeyDown}>
+        <button
+          onClick={this.handleClick}
+          
+        >
+          {this.state.selectedvalue}
          
-            
-          </button>
-          {this.state.popupVisible && (
-            <div
-              className="popover"  
-              
-            >
-              
-                <div 
-                tabIndex="-1"
-                  >
-               <button>Neptunium</button>
-               <button>Plutonium</button>
-               <button>Americium</button>
-               <button>Berkelium</button>
-              </div>
-            </div>
-           )}
-        </div>
+
+
+        </button>
+        {this.state.popupVisible && (
+          <div className="popover"
+         
+           >
+            <button  className={(this.state.selectedvalue=='Neptunium')?'active':''}>Neptunium</button>
+            <button  className={(this.state.selectedvalue=='Plutonium')?'active':''}>Plutonium</button>
+            <button  className={(this.state.selectedvalue=='Americium')?'active':''}>Americium</button>
+            <button  className={(this.state.selectedvalue=='Berkelium')?'active':''}>Berkelium</button>
+          </div>
+        )}
+      </div>
       );
     }
   }
-  
+ 
   ReactDOM.render(<App />, document.getElementById('root'));
